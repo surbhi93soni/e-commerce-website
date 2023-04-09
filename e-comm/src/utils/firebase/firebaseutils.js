88ae4c855223f626498572a9be8d803139ede0c1
unknown <sur.soni108@gmail.com>
2023-04-09 14:49:37 +0530

    changes done

---
+++ e-comm/src/utils/firebase/firebaseutils.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyArVUIsnG1i9gsBc0sd_EsIF_YSOqX-Jq4",
+  authDomain: "e-comm-project-fbb9f.firebaseapp.com",
+  projectId: "e-comm-project-fbb9f",
+  storageBucket: "e-comm-project-fbb9f.appspot.com",
+  messagingSenderId: "195601116395",
+  appId: "1:195601116395:web:a84942dd92ebf342bd62ae",
+  measurementId: "G-2C7H7BCSYQ"
+};
+
+// const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth ) => {
+  const userDocRef =doc(db , 'users' ,userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
